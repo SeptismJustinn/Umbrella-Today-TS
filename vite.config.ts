a9d2a0156f682dfdf845d8c7e52d9c90913ab56f
JustinnTanCK <81621295+SeptismJustinn@users.noreply.github.com>
2024-12-09 10:34:45 +0800

    Add vite-tsconfig-paths
    Import paths configured in tsconfig are not automatically detected by vite.config and have to be configured via resolve.alias. Install vite-tsconfig-paths module as dev dependency to automatically read paths from tsconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigpaths from "vite-tsconfig-paths"
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigpaths()],
   // server: {
   //   port:3456,
   //   open: true,
