030295e81b0b726bd01e0e0f091fb8d7d1ebed07
JustinnTanCK <81621295+SeptismJustinn@users.noreply.github.com>
2024-12-09 16:06:43 +0800

    Added DataContext context file
    Data fetched upon App load and distributed among pages to minimize fetching, particularly when going back to main page from another page.

---
+++ src/helpers/DataContext.tsx
@@ -0,0 +1,26 @@
+import { CoordArray, Fetched } from "common-types";
+import React from "react";
+
+export interface DataContextProps {
+  data: Fetched.Dataseries;
+  date: Date;
+  coords: CoordArray;
+  setCoords: Function;
+  outdated: boolean;
+  setOutdated: Function;
+  loading: boolean;
+  setLoading: Function;
+}
+
+const DataContext = React.createContext<DataContextProps>({
+  data: [],
+  date: new Date(),
+  coords: [0, 0],
+  setCoords: () => {},
+  outdated: false,
+  setOutdated: () => {},
+  loading: true,
+  setLoading: () => {},
+});
+
+export default DataContext;
